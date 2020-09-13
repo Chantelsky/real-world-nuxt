97459e7b3df7324972cfc3a93bf6ea0ed9b8ef63
Chantelsky <chantel.sky@outlook.com>
2020-09-13 18:41:06 +1000

    file-based routing

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="nav">
-        <nuxt-link to="/" class="brand">Real World Events</nuxt-link>
-        <nav>
-            <nuxt-link to="/">List</nuxt-link> |
-            <nuxt-link to="/create">Create</nuxt-link>
-        </nav>
-    </div>
+  <div class="nav">
+    <nuxt-link to="/" class="brand">Real World Events</nuxt-link>
+    <nav>
+      <nuxt-link to="/">List</nuxt-link> |
+      <nuxt-link to="/event/create">Create</nuxt-link>
+    </nav>
+  </div>
 </template>
 
 <style scoped>
 .brand {
-  font-family: "Montserrat", sans-serif;
+  font-family: 'Montserrat', sans-serif;
   font-weight: 700;
   font-size: 1.5em;
   color: #39b982;
